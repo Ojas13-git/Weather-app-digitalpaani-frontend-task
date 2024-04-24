088fdf38860a04fe8934f56a9e38e60a2ba5598c
Ojas13-git <79032848+Ojas13-git@users.noreply.github.com>
2024-04-25 01:23:30 +0530

    Pushing initial files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React,{useState} from 'react';
+import axios from 'axios';
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
